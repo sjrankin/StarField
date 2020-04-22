0bbc14dfd4dba914a17187743fe9fba12c60efcf
Stuart Rankin <sjrankin@gmail.com>
2020-04-22 21:55:55 +0900

    Update ViewController.swift
    Handle UI for velocity. Fills the stars from viewDidLoad.

--- Starfield/ViewController.swift
+++ Starfield/ViewController.swift
@@ -7,17 +7,68 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
 {
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        StarFieldViewer.layer.borderColor = UIColor.systemYellow.cgColor
-        StarFieldViewer.layer.borderWidth = 4.0
-        StarFieldViewer.layer.cornerRadius = 10.0
+        StarFieldViewer.FillStars(To: 1000, WithSpeed: 1.0)
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle
+    {
+        return .lightContent
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int
+    {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        switch pickerView
+        {
+            case VelocityPicker:
+                return VelocityValues.count
+            
+            default:
+            return 0
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        switch pickerView
+        {
+            case VelocityPicker:
+            return "\(VelocityValues[row])"
+            
+            default:
+            return nil
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+    switch pickerView
+    {
+        case VelocityPicker:
+        StarFieldViewer.ChangeSpeed(To: VelocityValues[row])
+        
+        default:
+        return
+        }
     }
 
+    var VelocityValues = [0.0, 0.25, 0.50, 0.75,
+                          1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0,
+                          20.0, 30.0, 50.0, 100.0]
+    
+
+    @IBOutlet weak var VelocityPicker: UIPickerView!
     @IBOutlet weak var StarFieldViewer: Starfield!
 }
 
