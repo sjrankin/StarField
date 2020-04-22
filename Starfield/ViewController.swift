94a66a04663a7bf48a1604ec6f4f78e33d43112b
Stuart Rankin <sjrankin@gmail.com>
2020-04-22 12:02:10 +0900

    Initial Commit

---
+++ Starfield/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Starfield
+//
+//  Created by Stuart Rankin on 4/22/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
